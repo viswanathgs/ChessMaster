b5d2cb6535474e184bfe032c359e77e7925d4f5b
Viswanath S <viswanathgs@gmail.com>
2011-04-15 18:36:43 +0530

    Schema definition - got nothing for pause and timers. Sunday night.

---
+++ db_schema.sql
@@ -0,0 +1,120 @@
+-- MySQL dump 10.13  Distrib 5.1.49, for debian-linux-gnu (i686)
+--
+-- Host: localhost    Database: chess
+-- ------------------------------------------------------
+-- Server version	5.1.49-1ubuntu8.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Chats`
+--
+
+DROP TABLE IF EXISTS `Chats`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Chats` (
+  `messageid` int(11) NOT NULL AUTO_INCREMENT,
+  `gameid` int(11) DEFAULT NULL,
+  `username` varchar(30) DEFAULT NULL,
+  `postedon` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `message` varchar(255) DEFAULT NULL,
+  `readflag` int(11) DEFAULT NULL,
+  PRIMARY KEY (`messageid`),
+  KEY `gameid` (`gameid`),
+  KEY `username` (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Games`
+--
+
+DROP TABLE IF EXISTS `Games`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Games` (
+  `gameid` int(11) NOT NULL AUTO_INCREMENT,
+  `player1` varchar(30) DEFAULT NULL,
+  `player2` varchar(30) DEFAULT NULL,
+  `turn` varchar(30) DEFAULT NULL,
+  `changed` int(11) DEFAULT NULL,
+  `board` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`gameid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `GamesHistory`
+--
+
+DROP TABLE IF EXISTS `GamesHistory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `GamesHistory` (
+  `gameid` int(11) DEFAULT NULL,
+  `fromsquare` varchar(10) DEFAULT NULL,
+  `tosquare` varchar(10) DEFAULT NULL,
+  `playedon` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY `gameid` (`gameid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Users` (
+  `username` varchar(30) NOT NULL DEFAULT '',
+  `aboutme` varchar(255) DEFAULT NULL,
+  `country` varchar(30) DEFAULT NULL,
+  `gamecount` int(11) DEFAULT NULL,
+  `wincount` int(11) DEFAULT NULL,
+  `losecount` int(11) DEFAULT NULL,
+  `drawcount` int(11) DEFAULT NULL,
+  `rank` int(11) DEFAULT NULL,
+  `status` varchar(10) DEFAULT NULL,
+  `gameid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`username`),
+  KEY `gameid` (`gameid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `UsersHistory`
+--
+
+DROP TABLE IF EXISTS `UsersHistory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `UsersHistory` (
+  `username` varchar(30) DEFAULT NULL,
+  `opponent` varchar(30) DEFAULT NULL,
+  `playedon` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `result` varchar(30) DEFAULT NULL,
+  KEY `username` (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2011-04-15 18:18:50
